a2a32c0ce026764190c36211eca666ad399573da
Claes Ö <claes@stenbergit.se>
2019-03-14 08:05:56 +0100

    Add json support.

--- PluginQrPhp_qr_code_1_1_4.php
+++ PluginQrPhp_qr_code_1_1_4.php
@@ -3,15 +3,25 @@ class PluginQrPhp_qr_code_1_1_4{
   public function widget_png($data){
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
-    $text = $data->get('data/text');
-    $text = urlencode($text);
+    $text = null;
+    if($data->get('data/text')){
+      $text = urlencode($data->get('data/text'));
+    }elseif($data->get('data/json')){
+      $text = $data->get('data/json');
+      $text = json_encode($text);
+      $text = urlencode($text);
+    }else{
+      $text = urlencode('Missing data.');
+    }
+    $src = '/qr/png?text='.$text;
     $img = wfDocument::createHtmlElementAsObject('img');
-    $img->set('attribute/src', '/qr/png?text='.$text);
+    $img->set('attribute/src', $src);
     wfDocument::renderElement(array($img->get()));
   }
   public function page_png(){
     require_once __DIR__.'/lib/qrlib.php';
     $text = wfRequest::get('text');
+    $text = urldecode($text);
     QRcode::png($text);
     exit;
   }
--- README.md
+++ README.md
@@ -36,3 +36,31 @@ Or just set img attribute src to:
 ```
 /qr/png?text=Some+text.
 ```
+
+### Json 
+
+Replace text param with json to put json data in qr image.
+
+```
+type: widget
+data:
+  plugin: qr/php_qr_code_1_1_4
+  method: png
+  data:
+    json:
+      name: James Smith
+      phone: '555-5555'
+```
+
+_Check out php code for widget how to build an json url._
+
+
+Outputs.
+
+
+```
+<img src="/qr/png?text=%7B%22name%22%3A%22James+Smith%22%2C%22phone%22%3A%22555-5555%22%7D">
+```
+
+
+
